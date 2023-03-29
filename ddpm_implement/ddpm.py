2a6978a3a75b3f15e2c3f06404848c4047c296af
o2rabbit <gih5444@naver.com>
2023-03-29 20:14:00 +0900

    ddpm study

--- ddpm_implement/ddpm.py
+++ ddpm_implement/ddpm.py
@@ -206,7 +206,7 @@ building block modules
 
 # Resnet 내 block modules
 class Block(nn.Module):
-    def __init__(self, dim, dim_out, groups=8): # group parameter 의미??
+    def __init__(self, dim, dim_out, groups=8): # group 의미??
         super().__init__()
         self.proj = WeightStandardizedConv2d(dim, dim_out, 3, padding=1)
         self.norm = nn.GroupNorm(groups, dim_out) # WeightStandardizedConv 를 쓴 이후에 다시 GroupNorm 을 쓰는 이유는???
@@ -316,13 +316,114 @@ class Unet(nn.Module):
 
         init_dim = default(init_dim, dim) # init dim 이 있으면 init dim, 아니면 dim
         self.init_conv = nn.Conv2d(input_channels, init_dim, 7, padding=3)
+        
+        dims = [init_dim, *map(lambda m: dim * m, dim_mults)] # 결과 : [init_dim, dim_mults[0] * dim, dim_mults[1] * dim, ...]
+        in_out = list(zip(dims[:-1], dims[1:])) # 각 block 의 in_dimension, out_dimension 을 만든다
 
-        dims = [init_dim, *map(lambda m: dim * m, dim_mults)]
-        in_out = list(zip(dims[:-1], dims[1:]))
+        block_klass = partial(ResnetBlock, groups=resnet_block_groups) # Resnet block을 그대로 새로운 함수 구현
 
-        block_klass = partial(ResnetBlock, groups=resnet_block_groups)
+        # time embedding
+        time_dim = dim * 4
+        self.random_or_learned_sinusoidal_cond = learned_sinsoidal_cond or random_fourier_features
 
+        if self.random_or_learned_sinusoidal_cond:
+            sinu_pos_emb = RandomOrLearnedSinusoidalEmb(learned_sinsoidal_dim, random_fourier_features)
+            fourier_dim = learned_sinsoidal_dim + 1
+        else:
+            sinu_pos_emb = SinusoidalPosEmb(dim)
+            fourier_dim = dim
 
+        self.time_mlp = nn.Sequential(sinu_pos_emb,
+                                      nn.Linear(fourier_dim, time_dim),
+                                      nn.GELU(),
+                                      nn.Linear(time_dim, time_dim))
+
+        # layers
+        self.downs = nn.ModuleList([])
+        self.ups = nn.ModuleList([])
+        num_resolutions = len(in_out) # in_out 의 길이 = unet block 의 수
+
+        # donw blocks
+        for ind, (dim_in, dim_out) in enumerate(in_out):
+            is_last = ind >= (num_resolutions - 1)
+
+            self.downs.append(nn.ModuleList([
+                block_klass(dim_in, dim_in, time_emb_dim=time_dim),
+                block_klass(dim_in, dim_in, time_emb_dim=time_dim),
+                # PreNorm 으로 LayerNorm 한 후 LinearAttention 수행
+                # Residual 로 PreNorm(dim_in) + dim_in 
+                Residual(PreNorm(dim_in, LinearAttention(dim_in))),
+                down_sample(dim_in, dim_out) if not is_last else nn.Conv2d(dim_in, dim_out, 3, padding=1)
+            ]))
+
+        # middle block
+        mid_dim = dims[-1]
+        self.mid_block1 = block_klass(mid_dim, mid_dim, time_emb_dim=time_dim)
+        self.mid_attn = Residual(PreNorm(mid_dim, Attention(mid_dim)))
+        self.mid_block2 = block_klass(mid_dim, mid_dim, time_emb_dim=time_dim)
+
+        # up block
+        for ind, (dim_in, dim_out) in enumerate(reversed(in_out)):
+            is_last = ind == (len(in_out) - 1) # reversed(in_out) 의 index 가 마지막 index 인지 확인하는 값
+
+            self.ups.append(nn.ModuleList([
+                block_klass(dim_out + dim_in, dim_out, time_emb_dim=time_dim), # Unet 구조이므로 input_ch 수는 donw layer 의 dim_out + mid layer 의 dim_out == dim_in
+                block_klass(dim_out + dim_in, dim_out, time_emb_dim=time_dim),
+                Residual(PreNorm(dim_out, LinearAttention(dim_out))),
+                up_sample(dim_out, dim_in) if not is_last else nn.Conv2d(dim_out, dim_in, 3, padding=1) # 마지막 layer 일 때는 conv
+            ]))
+
+        default_out_dim = channels * (1 if not learned_variance else 2)
+        self.out_dim = default(out_dim, default_out_dim)
+
+        self.fianl_res_block = block_klass(dim * 2, dim, time_emb_dim=time_dim)
+        self.final_conv = nn.Conv2d(dim, self.dim, 1)
+
+    def forward(self, x, time, x_self_cond=None):
+        if self.self_condition:
+            x_self_cond = default(x_self_cond, lambda: torch.zeros_like(x))
+            x = torch.cat((x_self_cond, x), dim=1)
+
+        x = self.init_conv(x)
+        r = x.clone()
+        t = self.time_mlp(time)
+        h = [] # up layer 로 보낼 값
+
+        # down
+        for block1, block2, attn, domwnsample in self.downs:
+            x = block1(x, t)
+            h.append(x)
+
+            x = block2(x, t)
+            x = attn(x) # donw ModuleList 에서 Residual
+            h.append(x)
+
+            x = domwnsample(x)
+        
+        # mid
+        x = self.mid_block1(x, t)
+        x = self.mid_attn(x)
+        x = self.mid_block2(x, t)
+
+        # up
+        for block1, block2, attn, upsample in self.ups:
+            x = torch.cat((x, h.pop()), dim=1)
+            x = block1(x, t)
+
+            x = torch.cat((x, h.pop()), dim=1)
+            x = block2(x, t)
+            x = attn(x)
+
+            x = upsample(x)
+
+        x = torch.cat((x, r), dim=1)
+        x = self.fianl_res_block(x)
+
+        return self.final_conv(x)
+    
+"""
+Gaussian Diffusion Trainer Class
+"""
 
 if __name__ == "__main__":
     pass
